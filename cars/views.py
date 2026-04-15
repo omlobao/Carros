64030061b0ef13a3c991f94680a831b930acde27
leish <omatheuslobao@gmail.com>
2026-04-14 23:29:06 -0300

    first commit

---
+++ cars/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from cars.models import Car
+
+def cars_view(request):
+    cars = Car.objects.filter(model__contains='ma')
+
+
+    return render (request, 
+                  'cars.html', 
+                   {'cars': cars}
+                    )
+
+
